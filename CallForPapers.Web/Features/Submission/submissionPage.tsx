1573ed05e1d2333d106cdf29b4789f31e55a9098
Geir Sagberg <geir.sagberg@gmail.com>
2017-09-23 16:42:32 +0200

    TypeScript WIP

---
+++ CallForPapers.Web/Features/Submission/submissionPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Form extends React.Component {
+  render () {
+    return <div />
+  }
+}
+
+ReactDOM.render(<Form />, document.getElementById('app'))
---
+++ CallForPapers.Web/webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	entry: 'app',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: 'wwwroot/dist'
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+
+				use: [
+					{
+						loader: 'style-loader',
+
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'css-loader'
+					}
+				]
+			}
+		]
+	},
+
+	plugins: [new UglifyJSPlugin()]
+};
